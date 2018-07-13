0835fdc8e1bd33d5bdc089864dd01390862dae71
pepaproch <pepaproch@gmail.com>
2018-07-13 17:23:54 +0200

    Initial commit

---
+++ src/app/App.kt
@@ -0,0 +1,33 @@
+package app
+
+import logo.logo
+import react.RBuilder
+import react.RComponent
+import react.RProps
+import react.RState
+import react.dom.code
+import react.dom.div
+import react.dom.h1
+import react.dom.p
+import ticker.ticker
+
+class App : RComponent<RProps, RState>() {
+    override fun RBuilder.render() {
+        div("App-header") {
+            logo()
+            h1 {
+                +"Wdelcome to React with Kotlin"
+            }
+        }
+        p("App-intro") {
+            +"To get started, edit "
+            code { +"ddapp/App.kt" }
+            +" and save to reload."
+        }
+        p("App-ticker") {
+            ticker()
+        }
+    }
+}
+
+fun RBuilder.app() = child(App::class) {}
